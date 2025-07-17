f14290a5e4335530721aee3adc2c25a28161eba2
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-07-18 00:15:54 +0800

    explicitly enable

--- configuration.nix
+++ configuration.nix
@@ -42,13 +42,13 @@
   ];
 
   # temporarily disable ipv6
-  # boot.kernel.sysctl = {
-  #   "net.ipv6.conf.all.disable_ipv6" = 1;
-  #   "net.ipv6.conf.default.disable_ipv6" = 1;
-  # };
+  boot.kernel.sysctl = {
+    "net.ipv6.conf.all.disable_ipv6" = 0;
+    "net.ipv6.conf.default.disable_ipv6" = 0;
+  };
 
   # time zone.
-  time.timeZone = "Asia/Shanghai";
+  time.timeZone = "Europe/Zurich";
 
   # language
   i18n.defaultLocale = "en_US.UTF-8";
