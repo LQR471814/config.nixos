a21600f4e810e98a81ee4f037675aea5c56911e4
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-07-27 12:59:43 -0700

    update syncthing script

--- setup-home.sh
+++ setup-home.sh
@@ -10,9 +10,6 @@
 # connct to wireguard if not on local network
 wg-up
 
-# pull syncthing secrets
-syncthing-cfg-pull
-
 # enable unfree packages & experimental nix features
 mkdir -p ~/.config/nixpkgs && echo "{ allowUnfree = true; }" > ~/.config/nixpkgs/config.nix
 mkdir -p ~/.config/nix
@@ -22,6 +19,11 @@ echo 'experimental-features = nix-command flakes' >> ~/.config/nix/nix.conf
 git clone https://github.com/LQR471814/config.home-manager ~/.config/home-manager
 cd ~/.config/home-manager
 make
+
+# pull syncthing secrets & reconfigure syncthing
+rm -rf ~/.local/state/syncthing
+syncthing-cfg-pull
+cd ~/.config/home-manager && make
 
 # setup nvim config
 git clone https://github.com/LQR471814/config.nivm ~/.config/nvim
