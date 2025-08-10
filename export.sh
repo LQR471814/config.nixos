24aeeeb70530eea12817672e28c13140fdb0c1ad
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-08-10 14:05:54 -0700

    fix dns prioritization

--- export.sh
+++ export.sh
@@ -1,3 +1,6 @@
 sudo cp * /etc/nixos
+
+intf=$(nmcli conn show HJHOME | grep "connection.interface-name:" | awk '{print $2}')
 nmcli -t conn show | grep -q "^HJHOME:" && \
-	sudo nmcli conn modify HJHOME ipv4.ignore-auto-dns yes ipv4.dns "192.168.1.10"
+	sudo nmcli conn modify HJHOME ipv4.ignore-auto-dns yes ipv4.dns "192.168.1.10" && \
+	sudo resolvectl domain $intf '~.'
