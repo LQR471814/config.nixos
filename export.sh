8915b399c0864450318775b6ba4cb2528ca98d9c
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-08-10 14:06:54 -0700

    update script

--- export.sh
+++ export.sh
@@ -1,6 +1,5 @@
 sudo cp * /etc/nixos
 
-intf=$(nmcli conn show HJHOME | grep "connection.interface-name:" | awk '{print $2}')
 nmcli -t conn show | grep -q "^HJHOME:" && \
 	sudo nmcli conn modify HJHOME ipv4.ignore-auto-dns yes ipv4.dns "192.168.1.10" && \
-	sudo resolvectl domain $intf '~.'
+	sudo resolvectl domain "$(nmcli conn show HJHOME | grep "connection.interface-name:" | awk '{print $2}')" '~.'
