2e0d6ad3c0e05eacb3131cea158fca7f25f60fbb
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-10-10 08:55:24 -0700

    feat: add flox

--- configuration.nix
+++ configuration.nix
@@ -288,10 +288,10 @@ lib.attrsets.recursiveUpdate
 
     nix.settings = {
       substituters = [
-        "https://nix-community.cachix.org"
+        "https://cache.flox.dev"
       ];
       trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
       ];
     };
 
