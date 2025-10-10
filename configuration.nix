777aa6ae919e4dc0e3e2252491efb5e44a0696a9
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-10-10 07:35:32 -0700

    feat: add cachix

--- configuration.nix
+++ configuration.nix
@@ -286,6 +286,15 @@ lib.attrsets.recursiveUpdate
       ./home_root.crt
     ];
 
+    nix.settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
     # Copy the NixOS configuration file and link it from the resulting system
     # (/run/current-system/configuration.nix). This is useful in case you
     # accidentally delete configuration.nix.
