000d0dbaf666ef40bb1033a49146242a8c477084
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-04-21 15:12:37 -0700

    feat: use xhost

--- overlays.nix
+++ overlays.nix
@@ -17,7 +17,7 @@ let
         mkdir -p $out/bin
         for file in ${pkg}/bin/*; do
           if [ -f "$file" ]; then
-            printf "${super.xorg.xhost}/bin/xhost +SI:localuser:root\n${super.lxqt.lxqt-sudo}/bin/lxqt-sudo '$file' ${flags}" | tee "$out/bin/$(basename "$file")"
+            printf "${super.xhost}/bin/xhost +SI:localuser:root\n${super.lxqt.lxqt-sudo}/bin/lxqt-sudo '$file' ${flags}" | tee "$out/bin/$(basename "$file")"
             chmod +x "$out/bin/$(basename "$file")"
           fi
         done
