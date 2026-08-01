efdc288b0ed507602a57bec3112fd441eb806dd7
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-08-01 13:27:03 -0700

    refactor(flake): system version

--- flake.nix
+++ flake.nix
@@ -23,7 +23,29 @@
       unstablePkgs = unstable.legacyPackages.${system};
 
       modules = [
-        ./configuration.nix
+        (_: {
+          # This option defines the first version of NixOS you have installed on this particular machine,
+          # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+          #
+          # Most users should NEVER change this value after the initial install, for any reason,
+          # even if you've upgraded your system to a new NixOS release.
+          #
+          # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+          # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+          # to actually do that.
+          #
+          # This value being lower than the current NixOS release does NOT mean your system is
+          # out of date, out of support, or vulnerable.
+          #
+          # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+          # and migrated your data accordingly.
+          #
+          # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+          system.stateVersion = "25.05"; # Did you read the comment?
+        })
+
+        ./cfg/system.nix
+        ./cfg/users.nix
         ./cfg/hardware.nix
         ./cfg/audio.nix
         ./cfg/bluetooth.nix
@@ -37,8 +59,6 @@
         ./cfg/packages.nix
         ./cfg/printing.nix
         ./cfg/services.nix
-        ./cfg/system.nix
-        ./cfg/users.nix
         ./cfg/virt.nix
         ./cfg/wireshark.nix
 
@@ -65,7 +85,12 @@
       nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
         inherit system modules;
         specialArgs = {
-          inherit inputs system unstablePkgs IS_DESKTOP;
+          inherit
+            inputs
+            system
+            unstablePkgs
+            IS_DESKTOP
+            ;
         };
       };
     };
